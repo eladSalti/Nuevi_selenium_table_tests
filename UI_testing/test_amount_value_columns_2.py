ecbf7d420d75edd684d4f8e2f62c8b20547cf444
Elad Salti <elad.salti7@gmail.com>
2023-06-01 14:39:07 +0300

    Fixed tc2

--- UI_testing/test_amount_value_columns_2.py
+++ UI_testing/test_amount_value_columns_2.py
@@ -2,20 +2,21 @@ import pytest
 from selenium.webdriver.common.by import By
 from helper import validate_amount_value_format
 
-"""In this test we would like to check if the amount value columns support a decimal point separated number with 
-up to 3 digits after the decimal point."""
-
-
 class TestTransactions:
     @pytest.mark.amount_value_columns
     def test_amount_value_format(self, table_data):
-        # Check the format of the amount value for each cell in the amount value column
+        amount_values = []  # Create an empty list to store the values
+
+        # Collect valid values from the column
         for row in table_data:
             columns = row.find_elements(By.TAG_NAME, "td")
             if len(columns) >= 2:
-                amount_value = columns[1].text  # Assuming the amount value column is the second column (index 1)
-                assert validate_amount_value_format(amount_value), f"Invalid amount value format: {amount_value}"
-                # Validate the amount value format using the helper function validate_amount_value_format If the
-                # format is invalid, the assert statement will raise an AssertionError with an appropriate error
-                # message. The test will fail and the error message will indicate the specific amount value that
-                # failed the validation.
+                if validate_amount_value_format(columns[2].text):
+                    amount_values.append(columns[2].text)  # Add the value to the list
+
+        # Check if the list is empty
+        assert amount_values, "No amount values found in the column"
+
+        # Check the values using assert
+        for amount_value in amount_values:
+            assert validate_amount_value_format(amount_value), f"Invalid amount value format: {amount_value}"
